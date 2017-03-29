4bb0e1fc8b44df5c52a3f6218a0a811bc92f005b
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-03-29 12:23:28 +0200

    initial commit

---
+++ webpage/metadata.py
@@ -0,0 +1,11 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'Brixner Inschriften',
+    'author': 'Peter Andorfer',
+    'subtitle': 'Eine Datenbank zu mittelalterlichen Inschriften in und um Brixen',
+    'description': 'some text to come',
+    'github': 'https://github.com/acdh-oeaw/brin',
+    'purpose_de': 'Das Ziel des Projekts "Brixner Inschriften" ist die Publikation von Daten über Inschriften in Brixen.',
+    'purpose_en': 'The purpose of the project "Brixner Inschriften" is the publication of data about inscriptions in Brixen.'}
