7488e3affca879d22e1f01f454fa439cc0921832
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-04-02 17:57:14 +0200

    hopefully fixed lng-lat mess in edit_place.html

--- places/views.py
+++ places/views.py
@@ -21,16 +21,16 @@ class PlaceListView(generic.ListView):
 
 @login_required
 def create_place(request):
-	if request.method == "POST":
-		form = PlaceForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('places:place_list')
-		else:
-			return render(request, 'places/edit_places.html', {'form': form})
-	else:
-		form = PlaceForm()
-		return render(request, 'places/edit_places.html', {'form': form})
+    if request.method == "POST":
+        form = PlaceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('places:place_list')
+        else:
+            return render(request, 'places/edit_places.html', {'form': form})
+    else:
+        form = PlaceForm()
+        return render(request, 'places/edit_places.html', {'form': form})
 
 
 @login_required
@@ -47,21 +47,21 @@ def edit_place(request, pk):
             response = r.text
             responseJSON = json.loads(response)
             responseJSON = responseJSON['geonames']
-            form = PlaceForm(instance = instance)
+            form = PlaceForm(instance=instance)
             print(url)
             return render(
                 request, 'places/edit_places.html',
-                {'object':instance, 'form':form, 'responseJSON':responseJSON}
+                {'object': instance, 'form': form, 'responseJSON': responseJSON}
             )
         except requests.exceptions.RequestException as e:
             url = e
-        form = PlaceForm(instance = instance)
+        form = PlaceForm(instance=instance)
         print(url)
         responseJSON = "hansi"
-		#form = OrtForm({'geonames_id':123})
-        return render(request, 'places/edit_places.html',
-			{'object': instance, 'form' :form, 'responseJSON': responseJSON}
-			)
+        return render(
+            request, 'places/edit_places.html',
+            {'object': instance, 'form': form, 'responseJSON': responseJSON}
+        )
     else:
         form = PlaceForm(request.POST, instance=instance)
         if form.is_valid():
