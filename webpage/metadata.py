72b0a8f17e251dac3599e09312ada18ff2f551ab
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-09-14 16:33:18 +0200

    added short description

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -4,8 +4,8 @@
 PROJECT_METADATA = {
     'title': 'BRIN',
     'author': 'Peter Andorfer',
-    'subtitle': 'Brixner Inschriften',
-    'description': 'A web application to publish research data about public inscritption in Brixen',
+    'subtitle': 'The inscriptions of Brixen / Bressanone',
+    'description': 'The project „The inscriptions of Brixen / Bressanone. Survey of the epigraphic testimonies of the Middle Ages and the Early Modern Period” (2011-2013) aimed at photographing, transcribing and describing all inscriptions on the territory of the municipality of Brixen/Bressanone (South Tyrol) from the period between the 13th century and the year 1665. As a second step, we make the results of this work accessible in an online database.',
     'github': 'https://github.com/acdh-oeaw/brin',
     'purpose_de': 'Ziel dieser Anwendung ist die Publikation von Daten.',
     'purpose_en': 'The purpose of this project is to publish data.',
